525407e806f50185cf01617414079009da451190
Kyle Becker <gnefkow87@gmail.com>
2020-02-15 22:41:10 -0500

    Added the ability to scrape only certain keywords

--- models/articles.js
+++ models/articles.js
@@ -16,6 +16,6 @@ var ArticleSchema = new Schema({
   }
 });
 
-var Article = mongoose.model("Article",ArticleSchema);
+var Article = mongoose.model("Article", ArticleSchema);
 
-module.exports= Article;
+module.exports = Article;
